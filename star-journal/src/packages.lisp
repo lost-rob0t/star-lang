f2dbc6df058f2787d3ebbe33fc08fecb4f2fb6a4
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-15 10:27:13 -0400

    Expose final runtime journal API

--- star-journal/src/packages.lisp
+++ star-journal/src/packages.lisp
@@ -1,4 +1,12 @@
 (defpackage :starjournal
   (:use :cl)
   (:nicknames :star-journal)
-  (:export))
+  (:export
+   #:star-journal-error
+   #:runtime-journal-port
+   #:runtime-journal-port-p
+   #:make-runtime-journal-port
+   #:runtime-journal-append
+   #:runtime-journal-replay
+   #:make-memory-runtime-journal-port
+   #:make-file-runtime-journal-port))
