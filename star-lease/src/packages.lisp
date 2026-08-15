b0ced27c10f4986df79374bd002690c882df152e
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-15 10:33:49 -0400

    Expose heartbeat lease primitive

--- star-lease/src/packages.lisp
+++ star-lease/src/packages.lisp
@@ -1,4 +1,12 @@
 (defpackage :starlease
   (:use :cl)
   (:nicknames :star-lease)
-  (:export))
+  (:export
+   #:star-lease-error
+   #:heartbeat-lease
+   #:heartbeat-lease-p
+   #:make-heartbeat-lease
+   #:heartbeat-lease-timeout-ms
+   #:heartbeat-lease-note-seen
+   #:heartbeat-lease-last-seen-at
+   #:heartbeat-lease-expired-p))
